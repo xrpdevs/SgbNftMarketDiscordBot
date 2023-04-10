af910a33ef1a06a412a4dc5ba072972ebcd3597c
jp <jp@txt3.com>
2023-04-10 04:53:26 +0100

    Bit of code cleanup...

--- src/main/java/dev/mouradski/sgbnftbot/model/Network.java
+++ src/main/java/dev/mouradski/sgbnftbot/model/Network.java
@@ -1,16 +1,17 @@
 package dev.mouradski.sgbnftbot.model;
 
 public enum Network {
-    
+
     FLARE(14),
     SONGBIRD(19);
 
-    Integer value;
+    final Integer value;
 
     Network(Integer value) {
         this.value = value;
     }
 
+    @SuppressWarnings("unused")
     public Integer getValue() {
         return this.value;
     }
